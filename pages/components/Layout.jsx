15165825e067430788410688c0109001d6e151bb
dzenan <dzenankrlic@gmail.com>
2022-03-26 18:36:41 +0100

    import navbar

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
  import { Box } from '@chakra-ui/react';
+ import Navbar from "./Navbar";
 
  const Layout = ({ children }) => (
      <>
@@ -8,7 +9,7 @@ import Head from "next/head";
         </Head>
         <Box maxWidth="1280px" m="auto">
             <header>
-                Navbar
+                <Navbar />
             </header>
             <main>
                 {children}

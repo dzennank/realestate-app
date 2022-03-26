9d7489e833b1612801744e207e9143e2d17e1380
dzenan <dzenankrlic@gmail.com>
2022-03-26 18:49:15 +0100

    ...

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
  import { Box } from '@chakra-ui/react';
  import Navbar from "./Navbar";
+ import Footer from "./Footer";
 
  const Layout = ({ children }) => (
      <>
@@ -15,7 +16,7 @@ import Head from "next/head";
                 {children}
             </main>
             <footer>
-                Footer
+                <Footer />
             </footer>
         </Box>
      </>

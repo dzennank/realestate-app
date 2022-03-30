ad1b99adad4f7be9040bb794e0ba3127e414eb2d
dzenan <dzenankrlic@gmail.com>
2022-03-30 18:28:21 +0200

    imageScrollbar component

---
+++ pages/components/imageScrollbar.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import Image from 'next/image';
+import { Box, Icon, Flex } from '@chakra-ui/react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
+
+const imageScrollbar = () => (
+    <ScrollMenu></ScrollMenu>
+)

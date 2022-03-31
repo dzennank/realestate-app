46b6fee158d9d501d1673853f4f110eac44cab1c
dzenan <dzenankrlic@gmail.com>
2022-03-31 19:11:03 +0200

    add imagescrollbar component

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -5,10 +5,11 @@ import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
+import imageScrollbar from '../components/imageScrollbar';
 
 const PropertyDetails = ({ propertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
     <Box maxWidth='1000px' margin='auto' p='4'>
-        {photos && <ImageScrollbar data={photos} />}
+        {photos && <imageScrollbar data={photos} />}
     </Box>
 )
 
